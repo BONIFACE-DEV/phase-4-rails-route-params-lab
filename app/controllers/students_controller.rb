83c62c619eb4b79f70ae1bc0cab44be79ebfe777
BONIFACE KORIR <boniface.korir@student.moringa.school@gmail.com>
2023-03-14 00:38:40 +0300

    done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,15 @@
 class StudentsController < ApplicationController
-
-  def index
-    students = Student.all
-    render json: students
+  def show
+    @student = Student.find(params[:id])
+    render json: {
+      id: @student.id,
+      first_name: @student.first_name,
+      last_name: @student.last_name,
+      grade: @student.grade
+    }
   end
-
+  def index
+    @students = Student.all
+    render json: @students
+  end
 end
